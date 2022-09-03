f8c87d0e4fb605a37ff8aabcb8a2f79e561c5708
Max <Max.Vinner@bk.ru>
2022-09-03 08:49:17 +0400

    addit_task_Bezmozgobot

---
+++ adit_task_robot/Program.cs
@@ -0,0 +1,26 @@
+﻿void robotcounter ()
+{
+try
+{
+    Console.Write("число кожаных мешков? ");
+    int number = int.Parse(Console.ReadLine());
+    if (number%10 == 5 || number%10 == 6 ||number%10 == 7 ||number%10 == 8 ||number%10 == 9 ||number%10 == 0 )
+    {
+        Console.WriteLine(number + " програмистов ");
+    }
+    else if (number%10 == 1)
+    {
+        Console.Write(number + " програмист ");
+    }
+    else
+    {
+        Console.Write(number + " програмистa ");
+    }
+}
+catch
+{
+    Console.Write("я не моч ");
+}
+}
+
+robotcounter ();
