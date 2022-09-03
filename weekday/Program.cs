197e1af7afbcfc7668f7f32fc124a4057a8f666b
Max <Max.Vinner@bk.ru>
2022-09-03 08:24:40 +0400

    weekday_task3

---
+++ weekday/Program.cs
@@ -0,0 +1,22 @@
+﻿void weekday ()
+{
+try
+{
+    Console.Write("введите цифру дня недели ");
+    int number = int.Parse(Console.ReadLine());
+    if (number == 6 || number==7)
+    {
+        Console.WriteLine("гуляем! ");
+    }
+    else 
+    {
+        Console.Write("эм... вроде не выходной. ну да ладно, гуляем!");
+    }
+}
+catch
+{
+    Console.Write("кажется мы друг друга недопоняли");
+}
+}
+
+weekday();
