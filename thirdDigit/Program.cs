0ebb154fc93359b85846d34c32ea5d65b54b1480
Max <Max.Vinner@bk.ru>
2022-09-03 08:14:13 +0400

    поиск третей  цифры трехзначного числа

---
+++ thirdDigit/Program.cs
@@ -0,0 +1,24 @@
+﻿void thirdDigit ()
+{
+try
+{
+    Console.Write("введите-Ка трехзначное число ");
+    int number = int.Parse(Console.ReadLine());
+    if (number-100 > 0&&number-1000 < 0)
+    {
+        Console.WriteLine("третья цифра - "+(number  % 10));
+    }
+    else 
+    {
+        Console.Write("число-То не треХзначное 0_о");
+    }
+}
+catch
+{
+    Console.Write("кажется мы друг друга недопоняли");
+}
+}
+
+thirdDigit ();
+
+
